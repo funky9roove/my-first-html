eb003063c114c235d75fd054cc964d1203827928
funky9roove <ytsdo4@gmail.com>
2022-06-08 17:30:04 +0900

    인포 테이블 작성

--- docs/index.js
+++ docs/index.js
@@ -4,9 +4,37 @@ let lastOpen;
 
 let infoTable = {
     '01': `<P>2022 Bahrain Grand Prix Winner</p>
-    <p>p1.Charles LECLERC<br>p2.Carlos SAINZ<br>p3.Lewis HAMILTON</p>`,
-    '02': "",
-    '03': "",
+    <img src="images/charles-leclerc.png" alt="error" width="130" height="130"><p>p1.Charles LECLERC</p>
+    <img src="images/Carlos-SAINZ.png" alt="error" width="193" height="130"><p>p2.Carlos SAINZ</p>
+    <img src="images/Lewis-HAMILTON.png" alt="error" width="200" height="130"><p>p3.Lewis HAMILTON</p>`,
+    '02': `<p>2022 Saudi Arabia Grand Prix Winner</P>
+    <img src="images/Max-VERSTAPPEN.png" alt="error" width="180" height="130"><p>p1.Max VERSTAPPEN</p>
+    <img src="images/charles-leclerc.png" alt="error" width="130" height="130"><p>p2.Charles LECLERC</p>
+    <img src="images/Carlos-SAINZ.png" alt="error" width="193" height="130"><p>p3.Carlos SAINZ</p>`,
+    '03': `<p>2022 Austraila Grand Prix Winner</p>
+    <img src="images/charles-leclerc.png" alt="error" width="130" height="130"><p>p1.Charles LECLERC</p>
+    <img src="" alt="error" width="" height=""><p>p2.Sergio PEREZ</p>
+    <img src="" alt="error" width="" height=""><p>p3.George RUSSELL</p>`,
+    '04': ``,
+    '05': ``,
+    '06': ``,
+    '07': ``,
+    '08': ``,
+    '09': ``,
+    '10': ``,
+    '11': ``,
+    '12': ``,
+    '13': ``,
+    '14': ``,
+    '15': ``,
+    '16': ``,
+    '17': ``,
+    '18': ``,
+    '19': ``,
+    '20': ``,
+    '21': ``,
+    '22': ``,
+    '23': ``,
 }
 
 
